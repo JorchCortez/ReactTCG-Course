3f56821de625bf88a9fe28ac4a22ef6d1f536384
Joorch <jacortez_94@hotmail.com>
2022-01-26 21:03:17 -0600

    [Update] Started working with hooks
    -Not a lot of progress today just hooks implementation (temporary)

--- project-1/src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ project-1/src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,23 @@
+import React, {useState} from 'react'
+
 import './ExpenseItem.css'
 import ExpenseDate from '../ExpenseDate/ExpenseDate'
 import Card from '../../UI/Card/Card'
 
 const ExpenseItem = (props) => { 
+
+    const [title, setTitle] = useState(props.title);
+
+    const onClickHandler = () =>{
+        setTitle("title was clicked")
+        alert("Clicked!")
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
-            <div className="expense-item__description" ><h2>{props.title}</h2></div>
+            <div className="expense-item__description" ><h2>{title}</h2></div>
+            <button onClick={onClickHandler}>Change Title</button>
             <div className="expense-item__price" >$ {props.amount}</div>
         </Card>
     )
