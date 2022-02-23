84d900fcfaf47893f4b396c76f4c8031069fea4e
Joorch <jacortez_94@hotmail.com>
2022-02-22 21:41:16 -0600

    refactoring

--- project-3/src/Components/UI/Button/Button.js
+++ project-3/src/Components/UI/Button/Button.js
@@ -4,7 +4,7 @@ import './Button.css'
 const Button = (props) => {
     return (
         <button 
-            className='btn' 
+            className='btn-default' 
             type={props.type || 'button'} 
             onClick={props.onClick}
         >
